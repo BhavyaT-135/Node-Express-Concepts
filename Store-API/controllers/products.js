b448cd872317a37c07e285afdc030b76a8f2abd1
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-03-07 13:20:07 +0530

    Sorting data in the get all products route

--- Store-API/controllers/products.js
+++ Store-API/controllers/products.js
@@ -8,7 +8,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -20,8 +20,19 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = {$regex: name, $options: 'i'}
     }
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+    // console.log(queryObject)
+    //With sort, we'll have to seperately handle the case where the sort value is not provided in the query
+    //because sort is chained to the find property and we would have to display all values if no sort query is available 
+    //Also, we have to cover the use case where we won't be returned anything from our await statement
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({products, nbHits: products.length})    
 }
 
