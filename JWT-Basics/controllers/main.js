05ce5228691db0afc3bfa5b93bd2b38f8e370252
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-03-18 23:21:59 +0530

    ✨ Testing JWT Token with payload, secret

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
@@ -9,8 +10,13 @@ const login = async (req, res) => {
         throw new CustomAPIError('Please provide username and password', 400)
     }
 
-    console.log(username, password);
-    res.send('Fake Login/Signup/Register Route')
+    // Just for Demo, normally provided by DB
+    const id = new Date().getDate()
+
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+    console.log(username, password, token);
+    res.status.json({msg:'user created', token})
 }
 
 const dashboard = async (req, res) => {
