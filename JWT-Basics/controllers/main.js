5bf019de00b58e167e02b6c024a332853ddd98b1
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-03-21 15:31:37 +0530

    ✨ Decoding jwt token

--- JWT-Basics/controllers/main.js
+++ JWT-Basics/controllers/main.js
@@ -27,7 +27,13 @@ const dashboard = async (req, res) => {
     }
 
     const token = authHeader.split(' ')[1]
-    console.log(token)
+    
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      console.log(decoded)
+    } catch (error) {
+      throw new CustomAPIError('Not authorized to access this route', 401)
+    }
 
     const luckyNumber = Math.floor(Math.random() * 100)
     res.status(200).json({msg: `Hello, Bhavya Tewari`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
