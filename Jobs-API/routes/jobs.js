e419aac7a8e48390d82c559d8e835e39ff652dc3
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-05 15:29:34 +0530

    ✨ Setting up routes in app.js

--- Jobs-API/routes/jobs.js
+++ Jobs-API/routes/jobs.js
@@ -1,4 +1,4 @@
-const express = requie('express')
+const express = require('express')
 const router = express.Router()
 
 const { getAllJobs, getJob, createJob, updateJob, deleteJob } = require('../controllers/jobs')
