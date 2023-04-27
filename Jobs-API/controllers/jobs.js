eb73828ed12158f755f457758d58c5b7a366a812
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2023-04-27 16:50:17 +0530

    ✨ Get single job controller

--- Jobs-API/controllers/jobs.js
+++ Jobs-API/controllers/jobs.js
@@ -4,12 +4,17 @@ const {BadRequestError, NotFoundError} = require('../errors')
 
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBY: req.user.userId }).sort('createdAt')
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length })  
 }
 
 const getJob = async (req, res) => {
-  res.send('Get job')
+  const { user: { userId }, params: { id: jobId } } = req
+  const job = await Job.findOne({ _id: jobId, createdBy: userId })
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+  res.status(StatusCodes.OK).json({ job })
 }
 
 const createJob = async (req, res) => {
